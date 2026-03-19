5e3251ee750ae31199e5d53e7b8dc9e0e4bd7df3
isgin01 <isgin01@gmail.com>
2026-03-19 16:59:59 +0500

    temp: custom view

--- src/custom-view.ts
+++ src/custom-view.ts
@@ -1,5 +1,6 @@
 import { ItemView, Workspace, WorkspaceLeaf } from "obsidian";
 import Timer from "timer";
+import type { TimeUpdateHandler } from "types";
 
 export const PLUGIN_CUSTOM_VIEW_ID = "better-pomodoro-view";
 
@@ -9,6 +10,54 @@ export class CustomView extends ItemView {
 	constructor(leaf: WorkspaceLeaf, timer: Timer) {
 		super(leaf);
 		this.timer = timer;
+		this.containerEl.empty();
+
+		// TODO: see if you can remove all these classes
+		var timeDisplayContainer = this.containerEl.createDiv({
+			cls: "timer-view-container",
+		});
+		var svgContainer = timeDisplayContainer.createDiv({
+			cls: "svg-container",
+		});
+
+		svgContainer.innerHTML = `
+			<svg xmlns="http://www.w3.org/2000/svg">
+			  <circle id="circle1" cx="70" cy="70" r="70" stroke-width="2" />
+			  <circle id="circle2" cx="70" cy="70" r="60" stroke-width="8" />
+			</svg>
+		`;
+
+		var buttonContainer = this.containerEl.createDiv({
+			cls: "button-container",
+		});
+
+		let HFTimeLeft = timer.getTimeLeft().HFTime;
+		var timeContainer = timeDisplayContainer.createSpan({
+			text: HFTimeLeft,
+			cls: "time-container",
+		});
+
+		var toggleButton = buttonContainer.createEl("button", {
+			text: "toggle",
+			cls: "toggle-button",
+		});
+		toggleButton.addEventListener("click", () => {
+			this.timer.toggle();
+		});
+
+		var resetButton = buttonContainer.createEl("button", {
+			text: "reset",
+			cls: "reset-button",
+		});
+		resetButton.addEventListener("click", () => {
+			this.timer.reset();
+		});
+
+		let updateHFTime: TimeUpdateHandler = (HFTime: string) => {
+			timeContainer.innerText = HFTime;
+		};
+
+		this.timer.registerTimeUpdateHandler(updateHFTime);
 	}
 
 	getViewType() {
@@ -18,19 +67,6 @@ export class CustomView extends ItemView {
 	getDisplayText() {
 		return "Pomodoro View";
 	}
-
-	onOpen: () => Promise<void> = async () => {
-		const container = this.containerEl;
-		container.empty();
-		container.createDiv();
-
-		container.createEl("button", { text: "toggle" }, () => {
-			this.timer.toggle();
-		});
-		container.createEl("button", { text: "reset" }, () => {
-			this.timer.reset();
-		});
-	};
 
 	// TODO: does it make sense to keep the func in the class?
 	async activate(workspace: Workspace) {
